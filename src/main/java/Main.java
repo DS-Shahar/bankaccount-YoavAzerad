9e831c61d9e5d043f71bded0798a473a0a23e1e3
YoavAzerad <yoavazerad2008@gmail.com>
2024-09-20 14:32:47 +0300

    Add files via upload

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,17 +3,16 @@ class Main
 { 
 	  Scanner reader = new Scanner(System.in);
 	  public static void main(String[] args) {
-	   
-		  BankAccount ba1 = new BankAccount("Bob", 1000, -1000);
-		  BankAccount ba2 = new BankAccount("Lisa", 1000, -1000);
-		  System.out.println(ba1.getName() + "'s balance is: " + ba1.getBalance());
-		  System.out.println(ba2.getName() + "'s balance is: " + ba2.getBalance());
-          ba1.transfer(ba1, ba2,500);
-          System.out.println(ba1.getName() + "'s balance is: " + ba1.getBalance());
-		  System.out.println(ba2.getName() + "'s balance is: " + ba2.getBalance());
-          ba1.transfer(ba1, ba2,1600);
-          System.out.println(ba1.getName() + "'s balance is: " + ba1.getBalance());
-		  System.out.println(ba2.getName() + "'s balance is: " + ba2.getBalance());
-		  ba1.toString();
-	  }
-}
+
+        Rectangle r1 = new Rectangle(11, 3);
+        r1.draw();
+        r1.calcArea();
+        r1.calcPerimeter();
+        System.out.println(r1.toString());
+        r1.scale(2);
+        r1.calcArea();
+
+
+
+      } 
+    }
